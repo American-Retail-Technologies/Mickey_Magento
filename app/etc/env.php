085f844ff5472ed36fc6cb94c3c64d03c89fd0cf
TenzinNKhando <tkhando@pioneer-inc.com>
2016-06-28 20:44:01 -0700

    adding all files, hoping it works

---
+++ app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin123',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'h2wttjcm00rsleg2mb2bj8fo84cbsq0t',
+  ),
+  'session' => 
+  array (
+    'save' => 'db',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => 'mgmv_',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'yooshc5_mage843',
+        'username' => 'yooshc5_mage843',
+        'password' => 'G2@6G3)PaS',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'full_page' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 23 Jun 2016 13:10:24 -0700',
+  ),
+);
